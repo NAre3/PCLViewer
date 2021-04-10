e00f79bd4a7697b767805be9ac534855dd110c85
azubu23202869 <43975713+azubu23202869@users.noreply.github.com>
2021-04-10 16:26:58 +0800

    加入專案檔案。

---
+++ QHeightRampDlg.h
@@ -0,0 +1,29 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+#include <QWidget>
+#include <QMessageBox>
+
+namespace Ui{
+	class QHeightRampDlg;
+}
+
+class QHeightRampDlg : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit QHeightRampDlg(QWidget* parent = 0);
+	~QHeightRampDlg();
+
+private:
+	Ui::QHeightRampDlg *ui;
+
+public Q_SLOTS:
+	void on_btn_ok_clicked();
+
+	void on_btn_cancel_clicked();
+
+signals:
+	void setHeightRamp(int dir, double height);
+
+};
